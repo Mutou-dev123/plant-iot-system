7bb7d352e31e57a2243bc9c973baa9f59028fee5
Yuga Kobayashi <yuga_kobayashi@ygcollege.jp>
2026-06-18 16:19:06 +0900

    Add esp32 develop folders and organaize folders

---
+++ esp32-sensor/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(4, HIGH);
+  Serial.println("VS CodeからLチカ！");
+  delay(2000);
+
+  digitalWrite(4, LOW);
+  Serial.println("VS CodeからLチカOFF！");
+  delay(2000);
+}
