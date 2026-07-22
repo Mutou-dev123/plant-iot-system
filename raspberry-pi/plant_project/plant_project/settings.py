14972c8f1b6c946fb45a74d667191770112a6f3a
Mutou-dev123 <kobayuga0219@icloud.com>
2026-07-22 18:02:43 +0900

    feat: create base template and update dashboard layout for improved UI

--- raspberry-pi/plant_project/plant_project/settings.py
+++ raspberry-pi/plant_project/plant_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zu=dm6)a)t+6p7d+brmhagofnnomrq7r$=-@t$ngv9(yymb#13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'plant_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
