6d4bba6e2056f5e3c574e76d19b135b453e01b6f
Mutou-dev123 <kobayuga0219@icloud.com>
2026-07-23 21:01:31 +0900

    feat: add static file handling and implement sidebar menu with associated styles and scripts

--- raspberry-pi/plant_project/plant_project/settings.py
+++ raspberry-pi/plant_project/plant_project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'plant_project.urls'
@@ -116,7 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)
 
 # すべてのモデルのIDをBigAutoFieldにする
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
